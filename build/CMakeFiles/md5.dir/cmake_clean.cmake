ab1db9193b5a5cf384d5921d74b1a9be902c6e78
steve <steve_chan@live.com.sg>
2022-05-15 11:21:08 +0800

    Add basic Conan files

---
+++ build/CMakeFiles/md5.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/md5.dir/md5.cpp.obj"
+  "CMakeFiles/md5.dir/md5.cpp.obj.d"
+  "bin/md5.exe"
+  "bin/md5.exe.manifest"
+  "bin/md5.pdb"
+  "lib/libmd5.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/md5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
